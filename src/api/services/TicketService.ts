ef908654a3cf92939b19a88b5a501f5076fdf846
jhennyf <jhennyfestudos2021@gmail.com>
2024-09-05 10:58:17 -0300

    :sparkles: feat: adicionando service do ticket

--- src/api/services/TicketService.ts
+++ src/api/services/TicketService.ts
@@ -6,22 +6,32 @@ export class TicketService {
     private ticketRepository = AppDataSource.getRepository(Ticket);
     
     async createTicket(ticket: Ticket) {
-        return this.ticketRepository.save(ticket);
+        
+        try {
+            return this.ticketRepository.save(ticket);
+        } catch (error) {   
+            return error;
+        }
     }
     
     async getTicketById(id: number) {
-        return this.ticketRepository.findOne(
+        const ticket = await this.ticketRepository.findOne(
             { where: { id } }
         );
+        if (ticket == null) {
+            throw new Error("Ticket not found");
+        }
+
+        return ticket; 
     }
     
     async getTickets() {
         return this.ticketRepository.find();
     }
     
-    async updateTicket(id: number, ticket: Ticket) {
-        return this.ticketRepository.update(id, ticket);
-    }
+        async updateTicket(id: number, ticket: Ticket) {
+            return this.ticketRepository.update(id, ticket);
+        }
     
     async deleteTicket(id: number) {
         return this.ticketRepository.delete(id);
