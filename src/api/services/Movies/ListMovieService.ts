c8481b8106dfa75425df74fc42ec9452a0d133ae
Erek Couto <erekcoutosantos22@gmail.com>
2024-09-06 15:55:21 -0300

    :recycle: refactor: verificar se há movies no banco de dados

--- src/api/services/Movies/ListMovieService.ts
+++ src/api/services/Movies/ListMovieService.ts
@@ -1,10 +1,15 @@
 import Movie from "../../../database/entities/Movie";
 import { AppDataSource } from "../../../database/";
+import AppError from "../../middlewares/AppError";
 
 class ListMovieService {
     public async execute(): Promise<Movie[]> {
         const movieRepository = AppDataSource.getRepository(Movie);
         const movie = await movieRepository.find();
+        
+        if(movie.length === 0) {
+            throw new AppError("There are no films registered.")
+        }
 
         return movie;
     }
