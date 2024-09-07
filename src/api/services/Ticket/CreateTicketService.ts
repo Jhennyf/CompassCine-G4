b317c32992beb34859b7a33b89ef6d5dcf644451
Felipe Rezende <felipe.rrezend@gmail.com>
2024-09-07 18:00:40 -0300

    :construction: feat: irá alterar o acesso e validação dos tickets.

--- src/api/services/Ticket/CreateTicketService.ts
+++ src/api/services/Ticket/CreateTicketService.ts
@@ -7,22 +7,52 @@ interface IRequest {
     chair: string;
 }
 
+interface IChairCount {
+    total: number;
+}
+
+interface ILimitSession {
+    capacity: number;
+}
+
+//conta o numero de cadeiras cadastradas
 class CreateTicketService {
+    async chairCount(): Promise<IChairCount[]> {
+        const ticketRepository = AppDataSource.getRepository(Ticket);
+        return ticketRepository
+            .createQueryBuilder("ticket")
+            .select("ticket.chair, COUNT(*) as total")
+            .groupBy("ticket.chair")
+            .getRawMany();
+    }
+
+    // async limitSession(): Promise<ILimitSession> {
+    //     const sessionRepository = AppDataSource.getRepository(Session);
+    //     return sessionRepository
+    //         .createQueryBuilder("session")
+    //         .select("session.sessionId, COUNT(*) as total")
+    //         .groupBy("ticket.chair")
+    //         .getRawMany();
+    // }
+
     public async execute({ value, chair }: IRequest): Promise<Ticket> {
         const ticketRepository = AppDataSource.getRepository(Ticket);
         const sessionRepository = AppDataSource.getRepository(Session);
 
-        const [chairCount] = await ticketRepository.findAndCount(chair)
+        // //conta o numero de cadeiras cadastradas
+        // const chairCount = await ticketRepository.count({ chair });
 
-        if(chairCount > sessionRepository.capacity){
+        //confere o numero de cadeiras com a capacidade da sessão
+        if (this.chairCount > sessionRepository.capacity) {
             throw new Error("Sold out.");
         }
 
+        //valida as cadeiras
         const chairExists = await ticketRepository.findOne({
-            where: { chair }
+            where: { chair },
         });
 
-        if (chairExists === true) {
+        if (chairExists) {
             throw new Error("Occupied chair.");
         }
 
